c3758d1e097455b7f6771eed3600888a97f34368
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-26 15:47:39 -0700

    test

--- Blue Quests/hello_world.cpp
+++ Blue Quests/hello_world.cpp
@@ -10,7 +10,7 @@ using namespace std;
 //  Feel free to use the alternate signature of main() below
 int main(int argc, const char * argv[]) 
 {
-  cout << "Hello world\n";
+  cout << "Hello world this is a test\n";
 
   return 0;
 }
