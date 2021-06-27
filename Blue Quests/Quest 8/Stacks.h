8ff0f0b5c44b816b56cdd772606343c6f0b7d981
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-26 18:52:50 -0700

    June 26, 2021 6:52 PM

--- Blue Quests/Quest 8/Stacks.h
+++ Blue Quests/Quest 8/Stacks.h
@@ -5,46 +5,115 @@
 #define Stacks_h
 #include <vector>
 #include <sstream>
-class Stack_Int
-{
-private:
-    std::vector<int> _data;
+class Stack_Int {
+    private:
+        std::vector<int> _data;
 
-public:
-    // No explicit constructor or destructor
-    size_t size() const
-    {
-        // TODO - Your code here
-    }
-    bool is_empty() const
-    {
-        // TODO - Your code here
-    }
-    void push(int val)
-    {
-        // TODO - Your code here
-    }
-    int top(bool &success) const
-    {
-        // TODO - Your code here
-    }
-    bool pop()
-    {
-        // TODO - Your code here
-    }
-    bool pop(int &val)
-    {
-        // TODO - Your code here
-    }
-    std::string to_string() const
-    {
-        // TODO - Your code here
-    }
-    // Don't remove the following line
-    friend class Tests;
+    public:
+        // No explicit constructor or destructor
+        size_t size() const
+        {
+            return _data.size();
+        }
+        bool is_empty() const
+        {
+            return _data.empty();
+        }
+        void push(int val)
+        {
+            _data.push_back(val);
+        }
+        int top(bool &success) const
+        {
+            if (_data.empty()) {success = false; return 0;}
+            else {success = true; return _data[_data.size()];}
+        }
+        bool pop()
+        {
+            if (_data.empty()) {return false;}
+            else {_data.pop_back(); return true;} 
+        }
+        bool pop(int &val)
+        {
+            if (_data.empty()) {return false;}
+            else {val = _data[_data.size()]; _data.pop_back(); return true;}
+        }
+        std::string to_string() const
+        {
+            std::string s = "";
+            std::string stackSize = std::to_string(_data.size());
+            s = s + "Stack (" + stackSize + " elements):\n";
+            if (_data.size() <=10) {
+                for (size_t i = 0; i < _data.size(); i++) {
+                    s += _data[i] + "\n";
+                }
+            }
+            else {
+                for (size_t j = _data.size()-10; j < _data.size(); j++) {
+                    s += _data[j] + "\n";
+                }
+                s += "...";
+            }
+            s += "Elements, if listed above, are in increasing order of age.";
+            return s;
+        }
+        // Don't remove the following line
+        friend class Tests;
 };
 class Stack_String
 {
-    // TODO - Your code here. Ask in the forums if you're stuck.
+        private:
+        std::vector<std::string> _data;
+
+    public:
+        // No explicit constructor or destructor
+        size_t size() const
+        {
+            return _data.size();
+        }
+        bool is_empty() const
+        {
+            return _data.empty();
+        }
+        void push(std::string val)
+        {
+            _data.push_back(val);
+        }
+        std::string top(bool &success) const
+        {
+            if (_data.empty()) {success = false; return 0;}
+            else {success = true; return _data[_data.size()];}
+        }
+        bool pop()
+        {
+            if (_data.empty()) {return false;}
+            else {_data.pop_back(); return true;} 
+        }
+        bool pop(std::string &val)
+        {
+            if (_data.empty()) {return false;}
+            else {val = _data[_data.size()]; _data.pop_back(); return true;}
+        }
+        std::string to_string() const
+        {
+            std::string s = "";
+            std::string stackSize = std::to_string(_data.size());
+            s = s + "Stack (" + stackSize + " elements):\n";
+            if (_data.size() <=10) {
+                for (size_t i = 0; i < _data.size(); i++) {
+                    s += _data[i] + "\n";
+                }
+            }
+            else {
+                for (size_t j = _data.size()-10; j < _data.size(); j++) {
+                    s += _data[j] + "\n";
+                }
+                s += "...";
+            }
+            s += "Elements, if listed above, are in increasing order of age.";
+            return s;
+        }
+        // Don't remove the following line
+        friend class Tests;
 };
 #endif /* Stacks_h */
