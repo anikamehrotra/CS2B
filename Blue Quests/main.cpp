24c8cfae18b1f87209cec5d71b0812ea9b0351b9
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-27 22:04:15 -0700

    June 27, 2021 10:04 PM

--- Blue Quests/main.cpp
+++ Blue Quests/main.cpp
@@ -4,13 +4,20 @@
 #include <string>
 
 // #include "Pet.cpp"
-#include "Quest 8/Stacks.h"
+#include "Quest 9/String_List.h"
 using namespace std;
 
 int main() {
-    Stack_Int s;
-    for (int i = 0; i < 1814; i++) {
-        s.push(i);
-    }
-    cout << s.to_string();
+    String_List n;
+    n.insert_at_current("a");
+    n.insert_at_current("b");
+    n.insert_at_current("c");
+    n.insert_at_current("d");
+    n.insert_at_current("e");
+    n.rewind();
+    n.advance_current();
+    n.advance_current();
+    n.insert_at_current("f");
+    cout << n.get_size();
+
 }
