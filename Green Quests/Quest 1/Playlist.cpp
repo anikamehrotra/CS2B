fb34d92ebd09abe8cb6904ad828eb25f5e9ae088
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-29 09:52:22 -0700

    June 29, 2021 9:52 AM

--- Green Quests/Quest 1/Playlist.cpp
+++ Green Quests/Quest 1/Playlist.cpp
@@ -68,7 +68,7 @@ Playlist *Playlist::remove_at_cursor() {
     if (_prev_to_current->get_next() == nullptr) {return this;}
     Node *current = _prev_to_current->get_next();
     if (_prev_to_current->get_next() == _tail) {_tail = _prev_to_current;}
-    _prev_to_current->insert_next(current->get_next());
+    // _prev_to_current->insert_next(current->get_next());
     if (current != nullptr) {
         delete current;
     }
