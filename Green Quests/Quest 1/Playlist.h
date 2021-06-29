d515251b3aa3adc8b1d30a5a615f256f6818f3bb
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-28 17:51:51 -0700

    June 28, 2021 5:51 PM

--- Green Quests/Quest 1/Playlist.h
+++ Green Quests/Quest 1/Playlist.h
@@ -66,7 +66,7 @@ class Playlist {
 
             public:
                 Node(const Song_Entry &song = Song_Entry()) : _song(song), _next(nullptr) {}
-                ~Node(); // Do not do recursive free
+                ~Node() {delete &_song;}; // Do not do recursive free
                 Song_Entry &get_song() { return _song; }
                 Node *get_next() { return _next; }
                 Node *insert_next(Node *p);
