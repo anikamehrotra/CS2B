35c2885623fff17a4bc3c07c26a2ef3279786358
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-28 19:02:52 -0700

    June 28, 2021 7:02 PM

--- Green Quests/main.cpp
+++ Green Quests/main.cpp
@@ -8,5 +8,15 @@
 using namespace std;
 
 int main() {
+    Playlist p;
+    Playlist::Song_Entry a(1, "a");
+    p.push_back(a);
+    Playlist::Song_Entry b(2, "b");
+    p.push_back(b);
+    Playlist::Song_Entry c(3, "c");
+    p.push_back(c);
+    Playlist::Song_Entry d(4, "d");
+    p.push_back(d);
+    cout << p.to_string();
 }
     
