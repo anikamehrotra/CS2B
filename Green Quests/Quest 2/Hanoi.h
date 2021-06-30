954c4418c12dc5eb60ca1f9c978119d141feb05f
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-29 23:42:09 -0700

    June 29, 2021 11:42 PM

--- Green Quests/Quest 2/Hanoi.h
+++ Green Quests/Quest 2/Hanoi.h
@@ -11,20 +11,20 @@
 using namespace std;
 
 class Hanoi {
-private:
-    int _num_poles;
-    int _num_discs;
-    // TODO: Declare the _cache member using an appropriate
-    // level of nesting within std::vectors to put each string
-    // of moves. You should be able to access the cache like so:
-    // _cache[num_discs][src][dst] = "move1\nmove2\n..."
-    std::string lookup_moves(int num_discs, int src, int dst);
-    std::string get_moves(int num_discs, int src, int dst, int tmp);
+    private:
+        int _num_poles;
+        int _num_discs;
+        // TODO: Declare the _cache member using an appropriate
+        // level of nesting within std::vectors to put each string
+        // of moves. You should be able to access the cache like so:
+        // _cache[num_discs][src][dst] = "move1\nmove2\n..."
+        std::string lookup_moves(int num_discs, int src, int dst);
+        std::string get_moves(int num_discs, int src, int dst, int tmp);
 
-public:
-    // Use freebie default constructor
-    std::string solve(int num_discs, int src, int dst, int tmp);
-    friend class Tests; // Don't remove this line
+    public:
+        // Use freebie default constructor
+        std::string solve(int num_discs, int src, int dst, int tmp);
+        friend class Tests; // Don't remove this line
 };
 
 #endif /* Hanoi_h */
