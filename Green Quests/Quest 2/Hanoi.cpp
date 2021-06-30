b366bc02eba287e38e22b30b47a90e5a7cbfc670
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-29 22:30:42 -0700

    June 29, 2021 10:30 PM

--- Green Quests/Quest 2/Hanoi.cpp
+++ Green Quests/Quest 2/Hanoi.cpp
@@ -6,3 +6,15 @@
 #include "Hanoi.h"
 
 using namespace std;
+
+void tower(int a,char from,char aux,char to){
+    if(a==1){
+       cout<<"\t\tMove disc 1 from "<<from<<" to "<<to<<"\n";
+       return;
+    }
+    else{
+       tower(a-1,from,to,aux);
+       cout<<"\t\tMove disc "<<a<<" from "<<from<<" to "<<to<<"\n";
+       tower(a-1,aux,from,to);
+    }
+}
