f546846c4d874b502bfd8b478b951393e72175b2
anikamehrotra <anikamehrotra2006@gmail.com>
2021-06-30 00:03:07 -0700

    June 30, 2021 12:03 AM

--- Green Quests/Quest 2/Hanoi.cpp
+++ Green Quests/Quest 2/Hanoi.cpp
@@ -16,10 +16,12 @@ std::string Hanoi::lookup_moves(int num_discs, int src, int dst) {
 
 std::string Hanoi::get_moves(int num_discs, int src, int dst, int tmp) {
     if (num_discs == 0) {return "";}
-    if (num_discs == 1) {return to_string(src) + " -> " + to_string(dst) + "\n";}
+    if (num_discs == 1) {
+        return std::to_string(src) + " -> " + std::to_string(dst) + "\n";
+    }
     return 
         get_moves(num_discs-1,src, tmp, dst) + 
-        to_string(src) + " -> " + to_string(dst) + "\n" + 
+        std::to_string(src) + " -> " + std::to_string(dst) + "\n" + 
         get_moves(num_discs-1, dst, src, tmp);
 }
 
