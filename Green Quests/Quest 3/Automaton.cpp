9a5fb5c0e86af75abe06111d151f79ea7c5de51c
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-06 21:51:06 -0700

    July 6, 2021 9:51 PM

--- Green Quests/Quest 3/Automaton.cpp
+++ Green Quests/Quest 3/Automaton.cpp
@@ -17,7 +17,7 @@ static size_t binary_to_decimal(const vector<int>& bits) {
  
     size_t len = bits.size();
     for (size_t i = len - 1; i >= 0; i--) {
-        if (bits.at(i) == '1')
+        if (bits[i] == 1)
             dec_value += base;
         base = base * 2;
     }
