1e0ed291366825710bc4c825bc777856812f3c05
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-06 20:51:49 -0700

    July 6, 2021 8:51 PM

--- Green Quests/Quest 3/Automaton.h
+++ Green Quests/Quest 3/Automaton.h
@@ -16,6 +16,23 @@ class Automaton {
         static const size_t MAX_PARENTS = 5;
         bool _is_valid;
         size_t _num_parents; // 2^{_num_parents} = possible parent combos
+
+        vector<bool> _rules; // size() = 2^q rules. one for each parent combo
+        int _extreme_bit;
+
+    public:
+        Automaton(size_t num_parents, size_t rule);
+        bool set_rule(size_t rule);
+        bool equals(const Automaton& that);
+
+        bool make_next_gen(const vector<int>& current_gen, vector<int>& next_gen);
+        string get_first_n_generations(size_t n =100, size_t width = 101);
+
+        // instance utility
+        string generation_to_string(const vector<int>& gen, size_t width);
+
+        // static utilities
+        static size_t pow_2(size_t n) {return 1 << n;}
 };
 
 #endif /* Automaton_h */
