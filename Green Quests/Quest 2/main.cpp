77a04a98f31e89d364fa53cdc7f23ed81c7b786e
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-12 22:30:17 -0700

    July 12, 2021 10:30 PM

--- Green Quests/Quest 2/main.cpp
+++ Green Quests/Quest 2/main.cpp
@@ -31,8 +31,7 @@ class Tests {
             for (int i = 1; i < h._cache.size(); i++) {
                 for (int j = 1; j < h._cache[i].size(); j++) {
                     for (int k = 1; k < h._cache[i][j].size(); k++) {
-                        s += "_cache["; 
-                        
+                        s += "_cache[" + to_string(i) + "][" + to_string(j) + "][" + to_string(k) + "] = " + h._cache[i][j][k] + "\n";                      
                         //+ i + i + "][" + j + "][" + k + "] = "
                          //    + h._cache[i][j][k] + "\n";
                     } 
@@ -47,18 +46,20 @@ int main() {
     Tests t;
     Hanoi h;
 
-    
+    /*
     t.hanoiTest(h, 0, 1, 2, 3, "");
     cout << t.cacheTest(h);
-
+    */
     t.hanoiTest(h, 1, 1, 2, 3, "1->2\n");
     cout << t.cacheTest(h);
+    /*
     t.hanoiTest(h, 2, 1, 2, 3, "1->3\n1->2\n3->2\n");
     t.hanoiTest(h, 3, 1, 2, 3, "1->2\n1->3\n2->3\n1->2\n3->1\n3->2\n1->2\n");
     t.hanoiTest(h, 3, 1, 2, 3, "1->2\n1->3\n2->3\n1->2\n3->1\n3->2\n1->2\n");
     
     t.hanoiTest(h, 2, 2, 1, 3, "2->3\n2->1\n3->1\n");
     t.hanoiTest(h, 2, 2, 1, 3, "2->3\n2->1\n3->1\n");
+*/  
 
     
 }
