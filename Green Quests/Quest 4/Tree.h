b32c085c8adc04626a877bd94be922ca5c25da2c
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-12 17:27:26 -0700

    July 12, 2021 5:27 PM

--- Green Quests/Quest 4/Tree.h
+++ Green Quests/Quest 4/Tree.h
@@ -36,9 +36,9 @@ class Tree
                    std::string new_data = that._data;
                    set_data(new_data);
                    Node *new_sibling = that._sibling;
-                   _sibling = new_sibling;
+                   *_sibling = *new_sibling;
                    Node *new_child = that._child;
-                   _child = new_child;
+                   *_child = *new_child;
                 }
                 return *this;
             };           // Deep clone
