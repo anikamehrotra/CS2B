83c10834d3e49410fb36f3d0d275f9575084c3c1
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-13 17:15:38 -0700

    July 13, 2021 5:15 PM

--- Green Quests/Quest 5/Complex.h
+++ Green Quests/Quest 5/Complex.h
@@ -32,24 +32,27 @@ class Complex {
         double norm_squared() const { return _real * _real + _imag * _imag;}; 
         double norm() const { return sqrt(norm_squared()); }
 
-        Complex reciprocal() const;
+        Complex reciprocal() const {
+            if (norm_squared() <= Complex::FLOOR) {throw Div_By_Zero_Execption();};
+            return Complex(_real / norm_squared(), -_imag / norm_squared());
+        };
         Complex& operator= (const Complex & rhs);
 
         class Div_By_Zero_Execption {
             public:
-                string to_string() const;
-                string what() const;
+                string to_string() const {return "Divide by zero exception";};
+                string what() const {return "Divide by zero exception";};
         };
 
         // operators (only the most common ones)
-        Complex operator+ (const Complex & that) const;
-        Complex operator- (const Complex & that) const;
-        Complex operator* (const Complex & that) const;
-        Complex operator/ (const Complex & that) const;
+        Complex operator+ (const Complex & that) const {return Complex(_real + that._real, _imag + that._imag);};
+        Complex operator- (const Complex & that) const {return Complex(_real - that._real, _imag - that._imag);};
+        Complex operator* (const Complex & that) const {return Complex(_real * that._real - _imag * that._imag, _real * that._imag + _imag * that._real);};
+        Complex operator/ (const Complex & that) const {if (that.norm_squared() <= FLOOR) {throw Div_By_Zero_Execption();} return *this * that.reciprocal();};
 
         bool operator==(const Complex & that) const {if (_real == that._real && _imag == that._imag) return true; else return false; };
         bool operator!=(const Complex & that) const { return !(*this == that);};
-        bool operator<(const Complex & that) const;
+        bool operator<(const Complex & that) const { return norm() < that.norm();};
 
         friend ostream& operator<< (ostream& os, const Complex& x);
         friend class Tests;
