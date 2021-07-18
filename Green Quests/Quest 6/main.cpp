245ffade52b9593b268cc5b43d6de7e689b02fa4
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-18 10:35:01 -0700

    July 18, 2021 10:35 AM

--- Green Quests/Quest 6/main.cpp
+++ Green Quests/Quest 6/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <assert.h>
 
 #include "Shapes.h"
 #include "Shapes.cpp"
@@ -9,15 +10,38 @@ using namespace std;
 
 class Tests {
     public:
-        void test_to_string(Screen& s, char c) {
-            s.fill(c);
-            s.to_string();
+
+        void test_fill_screen(Screen *s, char c, string expectedOutput) {
+            s->fill(c);
+            test_to_string(s, expectedOutput);
+        }
+
+        void test_to_string(Screen *s, string expectedOutput) {
+            string output = s->to_string();
+            assert(output == expectedOutput);
+            cout << expectedOutput << endl;
         } 
 };
 
 int main() {
     Tests t;
-    Screen s(3, 3);
-    s.fill('c');
-    cout << s.to_string();
+    string output;
+
+    t.test_fill_screen(new Screen(3, 3), 'c', "ccc\nccc\nccc\n");
+    t.test_fill_screen(new Screen(2, 1), 'c', "cc\n");
+    t.test_fill_screen(new Screen(1, 2), 'c', "c\nc\n");
+    t.test_fill_screen(new Screen(1, 1), 'c', "c\n");
+    t.test_fill_screen(new Screen(0, 0), 'c', "");
+
+
+/*
+    s = new Screen(0, 0); s->fill('c'); output = s->to_string();
+    assert(output == ""); 
+    cout << output << endl;
+    delete s;
+
+    s = new Screen(3, 3); s->fill('c'); output = s->to_string();
+    assert(output == "ccc\nccc\nccc\n");
+    cout << output << endl;
+    delete s; */
 }
