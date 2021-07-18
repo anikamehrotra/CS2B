c0b1938aa27c3577080fb68ce0add8946e480318
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-18 10:05:10 -0700

    July 18, 2021 10:05 AM

--- Green Quests/Quest 6/Shapes.cpp
+++ Green Quests/Quest 6/Shapes.cpp
@@ -10,7 +10,11 @@
 
 Screen::Screen(size_t w, size_t h) {
     _pix.resize(h);
-    _pix[h].resize(w);
+    for (size_t i = 0; i < h; i++) {
+        _pix[i].resize(w);
+    }
+    _w = w;
+    _h = h;
 }
 
 void Screen::fill(char c) {
