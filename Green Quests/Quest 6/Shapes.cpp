8de11fe064b44f9db4c51c841e0798f4a2e4ba93
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-18 10:19:11 -0700

    July 18, 2021 10:19 AM

--- Green Quests/Quest 6/Shapes.cpp
+++ Green Quests/Quest 6/Shapes.cpp
@@ -37,7 +37,9 @@ string Screen::to_string() const {
 }
 
 bool Point::draw(Screen &screen, char c) {
-    
+    if (_x < 0 || _x >= screen.get_w() || _y < 0 || _y >= screen.get_h()) {return false;}
+    screen.get_pix()[_y][_x] = c;
+    return true;
 }
 
 using namespace std;
