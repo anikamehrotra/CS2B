7acc882ad49d7d48c5f78cbd0012fdaaa7e4065f
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-20 17:11:07 -0700

    July 20, 2021 5:11 PM

---
+++ Green Quests/Quest 7/Queue.cpp
@@ -0,0 +1,12 @@
+// Student ID: 20480681
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include "Queue.h"
+using namespace std;
+
---
+++ Green Quests/Quest 7/Queue.h
@@ -0,0 +1,17 @@
+// Student ID: 20480681
+
+#ifndef Queue_h
+#define Queue_h
+
+#include <iostream>
+#include <sstream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+
+
+#endif /* Queue_h */
