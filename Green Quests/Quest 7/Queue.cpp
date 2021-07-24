0fd4707ed12a7d8115673cbc74e40fbc67d21df5
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-24 16:45:03 -0700

    July 24, 2021 4:45 PM

--- Green Quests/Quest 7/Queue.cpp
+++ Green Quests/Quest 7/Queue.cpp
@@ -9,3 +9,29 @@
 
 #include "Queue.h"
 using namespace std;
+
+/*
+Hooray! 1 Declarative Statement entered by King Photon VII in his Imperative Manifesto (ctr)
+
+Hooray! 4 Paintings of indranesque draw freeze mystified miscreants (enqueue)
+
+Hooray! 4 Filled Cups of Snapioca Tea made half the camp happy (dequeue)
+
+Hooray! 3 Levenshire Dwarves come up with seventeen cuning planes (peek)
+
+Hooray! 1 Bell-bottom has enough purple denim for 172 prufrocks (is_empty)
+
+You can keep going. Or enter the next quest. Or both.
+
+Hooray, you can follow photon - the king of tardigrades
+
+Hooray! 4 Malgonic Madmen decide to undo all their mad mischiefs (resize up)
+
+Hooray! 3 Scepters of Truthfulness restored to kings with kind hearts (resize down)
+
+Hooray! 2 T'grunhald Hyppoceros swayed by a sinewave at 172.9 QHz (popalot)
+
+Hooray! 3 Hebobs are no match for your modded borgen supercluster (to string)
+
+Hooray! 1 Parsimonia stewed until tender, then seasoned with seagrass (Queue)
+*/
