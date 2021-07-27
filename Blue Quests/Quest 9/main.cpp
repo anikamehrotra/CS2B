a44b9115e4135b4ab5e9cd4527440e0d05cecf41
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-27 13:52:16 -0700

    July 27, 2021 1:52 PM

---
+++ Blue Quests/Quest 9/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+// #include "Pet.cpp"
+#include "String_List.h"
+using namespace std;
+
+int main() {
+    String_List sl;
+    sl.insert_at_current("e");
+    sl.insert_at_current("d");
+    sl.insert_at_current("c");
+    sl.insert_at_current("b");
+    sl.insert_at_current("a");
+    sl.rewind();
+    sl.advance_current();
+    sl.advance_current();
+    sl.insert_at_current("f");
+    // sl.clear();
+    // cout << sl.get_size();
+    cout << sl.to_string();
+    sl.rewind();
+    cout << sl.to_string();
+    sl.clear();
+    cout << sl.to_string();
+}
