ea9585d7da104d4f609373da0ab495f8d2b7bda5
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-26 23:09:09 -0700

    July 26, 2021 11:09 PM

--- Green Quests/Quest 8/main.cpp
+++ Green Quests/Quest 8/main.cpp
@@ -15,5 +15,10 @@ class Tests {
 int main() {
     Trie t;
     t.insert("hello");
-    t.lookup("he");
+    t.insert("he");
+    cout << "lookup he: " << t.lookup("he") << endl;
+    cout << "lookup h: " << t.lookup("h") << endl;
+    cout << "lookup a: " << t.lookup("a") << endl;
+    cout << "lookup hello: " << t.lookup("hello") << endl;
+    cout << "lookup helloa: " << t.lookup("helloa") << endl;
 }
