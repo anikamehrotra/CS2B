25b7f6649f5808b00826b83d098c89e76b78b15f
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-27 09:08:25 -0700

    July 27, 2021 9:08 AM

--- Green Quests/Quest 8/main.cpp
+++ Green Quests/Quest 8/main.cpp
@@ -10,10 +10,16 @@ using namespace std;
 
 class Tests {
     public:
-       
+       void test_get_completions (Trie &t, string s, vector<string> &completions, size_t limit) {
+           t.get_completions(s, completions, limit);
+           for (size_t i = 0; i < completions.size(); i++) {
+               cout << " " << completions[i];
+           }
+       }
 };
 
 int main() {
+    Tests test;
     Trie t;
     t.insert("hello");
     t.insert("he");
@@ -24,5 +30,5 @@ int main() {
     cout << "lookup helloz: " << t.lookup("helloz") << endl;
     cout << t.to_string(100) << endl;
     vector<string> completions;
-    t.get_completions("s", completions, 1000);
+    test.test_get_completions(t, " ", completions, 1000);
 }
