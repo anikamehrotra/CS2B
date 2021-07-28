16527ec1ccdc9c7fb782bcc93c5d3a6d18a9bca4
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-28 13:01:57 -0700

    July 28, 2021 1:01 PM

--- Blue Quests/Quest 4/Looping_Functions.h
+++ Blue Quests/Quest 4/Looping_Functions.h
@@ -5,6 +5,9 @@
 // to copy it verbatim.
 #ifndef Looping_Functions_h
 #define Looping_Functions_h
+
+#include <string>
+using namespace std;
 // Declarations of the functions in looping_functions.cpp
 bool play_game(int n);
 double etox(double x, size_t n);
@@ -13,4 +16,5 @@ size_t gcd(size_t n1, size_t n2);
 std::string get_ap_terms(int a, int d, size_t n);
 std::string get_gp_terms(double a, double r, size_t n);
 double get_nth_fibonacci_number(size_t n);
+
 #endif /* Looping_Functions_h */
---
+++ Green Quests/Quest 9/Graph.cpp
@@ -0,0 +1,27 @@
+// Student ID: 20480681
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+
+#include "Graph.h"
+using namespace std;
+
+void add_edge(int src, int dst, std::string tag) {
+    
+};
+
+void make_silly_snake() {
+    Graph g;
+    g.addEdge(0, 1, "i-see");
+};
+void make_mr_sticky();
+void make_driftin_dragonfly();
+void make_slinky_star();
+void make_kathy_da_grate();
+void make_dodos_in_space();
+void make_purty_pitcher();
