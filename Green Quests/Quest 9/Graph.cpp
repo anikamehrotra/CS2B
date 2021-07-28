c30101ec71156f4d5f45a96ac7dbd80180f72004
anikamehrotra <anikamehrotra2006@gmail.com>
2021-07-28 13:27:48 -0700

    July 28, 2021 1:27 PM

--- Green Quests/Quest 9/Graph.cpp
+++ Green Quests/Quest 9/Graph.cpp
@@ -34,16 +34,74 @@ void Graph::make_mr_sticky() {
 void Graph::make_driftin_dragonfly() {
     _nodes.clear();
     add_edge(0, 1, "dis-be-me-head");
+    add_edge(1, 2, "me");
+    add_edge(2, 3, "long-tail");
+    add_edge(3, 4, "be-dis");
+
+    add_edge(1, 5, "dis-be-me");
+    add_edge(5, 6, "front-right");
+    add_edge(6, 7, "me-right");
+    add_edge(7, 8, "back");
+    add_edge(8, 2, "be-dis");
+
+    add_edge(1, 9, "dis-be-me");
+    add_edge(9, 10, "front-left");
+    add_edge(10, 11, "and-left");
+    add_edge(11, 12, "back");
+    add_edge(12, 2, "be-dis");
 };
 
 void Graph::make_slinky_star() {
     _nodes.clear();
+    add_edge(0, 1, "-");
+    add_edge(1, 2, "-");
+
+    add_edge(0, 3, "-");
+    add_edge(3, 4, "-");
+
+    add_edge(0, 5, "-");
+    add_edge(5, 6, "-");
+
+    add_edge(0, 7, "-");
+    add_edge(7, 8, "-");
+
+    add_edge(0, 9, "-");
+    add_edge(9, 10, "-");
+
+    add_edge(0, 11, "-");
+    add_edge(11, 12, "-");
+
+    add_edge(0, 13, "-");
+    add_edge(13, 14, "-");
 };
 void Graph::make_kathy_da_grate() {
     _nodes.clear();
+    add_edge(0, 1, "-");
+    add_edge(1, 2, "-");
+    add_edge(2, 3, "-");
+    add_edge(3, 4, "-");
+    add_edge(4, 0, "-");
+
+    add_edge(0, 5, "-");
+    add_edge(5, 6, "-");
+
+    add_edge(1, 7, "-");
+    add_edge(7, 8, "-");
+
+    add_edge(2, 9, "-");
+    add_edge(9, 10, "-");
+
+    add_edge(3, 11, "-");
+    add_edge(11, 12, "-");
+
+    add_edge(4, 13, "-");
+    add_edge(13, 14, "-");
 };
 void Graph::make_dodos_in_space() {
     _nodes.clear();
+    for (int i = 0; i < 50; i+=2) {
+        add_edge(i, i + 1, "Yippee-Dodo-" + i);
+    }
 };
 void Graph::make_purty_pitcher() {
     _nodes.clear();
--- Green Quests/Quest 9/Graph.h
+++ Green Quests/Quest 9/Graph.h
@@ -25,7 +25,7 @@ class Graph {
         // Suggested private helpers. Not tested.
         void add_edge(int src, int dst, std::string tag) {
             if (_nodes.size() <= (size_t) src) {_nodes.resize(src + 1);}
-            if (_nodes[src].size() <= (size_t) dst) {_nodes.resize(dst + 1);}
+            if (_nodes.size() <= (size_t) dst) {_nodes.resize(dst + 1);}
             _nodes[src].push_back(Edge(dst, tag));
         };
         std::string to_string() const;
